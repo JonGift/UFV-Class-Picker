316f5a45dadc8bd8732b5b42bb19f842050f84b9
ben <benhgift@gmail.com>
2014-12-18 14:33:01 -0800

    added better parsing that can get all the links

--- parser.py
+++ parser.py
@@ -2,14 +2,56 @@ import urllib2
 import re
 
 
+def get_data_from_site(site):
+    responce = urllib2.urlopen(site)
+    html = responce.read()
+    return html
+
+
+def store_data_locally(data, dest):
+    with open(dest, 'w+') as f:
+        f.write(data)
+
+
+def table_check(tab, line):
+    if tab:
+        if re.search('</tr>', line):
+            return False
+        else:
+            return True
+    else:
+        if re.search('<tr>', line):
+            return True
+        else:
+            return False
+
+
+def get_links(line):
+    return re.findall('(?:.*?href.+?\#)(.+?)(?=\")', line)
+
+
+def parse_ufv_links(text):
+    sub_names = []
+    in_table = False
+    for line in text:
+        in_table = table_check(in_table, line)
+        if in_table:
+            sub_names.append(get_links(line))
+    return [x for x in sub_names if x]
+
+
+def parse_ufv_data(source):
+    with open(source, 'r') as f:
+        text = f.readlines()
+    links = parse_ufv_links(text)
+
+
 def main():
-    response = urllib2.urlopen('http://www.ufv.ca/arfiles/includes/201501-timetable-with-changes.htm')
-    html = response.read()
-
-    with open("ufv_class_data.html", "w+") as f:
-        f.write(html)
-
-    print len(html.split('-' * 132))
+    site = 'http://www.ufv.ca/arfiles/includes/201501-timetable-with-changes.htm'
+    dest = 'ufv_class_data.html'
+    data = get_data_from_site(site)
+    store_data_locally(data, dest)
+    parse_ufv_data(dest)
 
 
 if __name__ == '__main__':
